8f9034ba1e856fd6a4fad8a2a13b516993a2825a
Julie Udoh <96840535+julieudoh@users.noreply.github.com>
2026-02-10 10:32:53 -0500

    Responsiveness
    Make the image in 4k screen size device bigger

--- src/components/intro/HeroIntro.vue
+++ src/components/intro/HeroIntro.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="w-small h-small mx-auto my-auto flex flex-col justify-center sm:w-large  text-center">
-    <h1 class="font-semibold text-[hsl(0,100%,100%)] text-[30px] xxsm:text-4xl sm:text-4xl md:text-[45px] lg:text-[60px] 2xl:text-[100px] 3xl:text-[130px]">
+  <div class="w-small h-small sm:h-large mx-auto flex flex-col justify-center sm:w-large  text-center">
+    <h1 class="font-semibold text-[hsl(0,100%,100%)] text-[30px] xxsm:text-4xl sm:text-4xl md:text-[40px] 2xl:text-[100px]">
       A modern publishing platform
     </h1>
-    <p class="font-light text-[#ffffff] text-lg xxsm:text-xl md:text-3xl 2xl:text-6xl 3xl:text-8xl pt-3 sm:pt-10 ">
+    <p class="font-light text-[#ffffff] text-lg xxsm:text-xl md:text-2xl 2xl:text-6xl pt-3 sm:pt-5 ">
       Grow your audience and build your online brand
     </p>
-    <div class="flex justify-center gap-2 sm:gap-4 lg:gap-10 mt-8 xxsm:mt-12  md:mt-18 xl:mt-22 2xl:mt-30 sm:mx-auto">
-      <base-button class="active w-32 sm:w-40 md:w-50 xl:w-60 2xl:w-80 3xl:w-90 h-10 sm:h-12 md:h-16 xl:h-20 2xl:h-25 3xl:h-30 border-[hsl(0,100%,100%)] text-[13px] xxsm:text-sm sm:text-16 md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl 3xl:text-6xl">Start for Free</base-button>
-      <base-button class="w-32 sm:w-40 md:w-50 xl:w-60 2xl:w-80 3xl:w-90 h-10 sm:h-12 md:h-16 xl:h-20 2xl:h-25 3xl:h-30 border-[hsl(0,100%,100%)] text-[13px] xxsm:text-sm sm:text-16 md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl 3xl:text-6xl">Learn More</base-button>
+    <div class="flex justify-center gap-2 sm:gap-4 lg:gap-10 mt-8 xxsm:mt-12  md:mt-10 xl:mt-22 2xl:mt-30 sm:mx-auto">
+      <base-button class="active w-32 sm:w-40 md:w-50 xl:w-60 2xl:w-90 h-10 sm:h-12 md:h-16 xl:h-20 2xl:h-30 border-[hsl(0,100%,100%)] text-[13px] xxsm:text-sm sm:text-16 md:text-xl xl:text-3xl 2xl:text-4xl">Start for Free</base-button>
+      <base-button class="w-32 sm:w-40 md:w-50 xl:w-60 2xl:w-90 h-10 sm:h-12 md:h-16 xl:h-20 2xl:h-30 border-[hsl(0,100%,100%)] text-[13px] xxsm:text-sm sm:text-16 md:text-xl xl:text-3xl 2xl:text-4xl">Learn More</base-button>
     </div>
   </div>
 </template>
--- src/components/nav/AppNav.vue
+++ src/components/nav/AppNav.vue
@@ -2,20 +2,20 @@
   <nav
     class="w-full shadow-sm bg-[hsl(0,100%,100%)] shadow-gray-100 rounded-m rounded-md p-4 sm:p-0 sm:bg-transparent lg:w-large sm:shadow-none sm:flex sm:items-center sm:top-0 sm:ml-7"
   >
-    <ul class="mb-2 sm:flex sm:justify-around sm:mb-0 sm:text-[14px] md:text-16 lg:text-xl xl:text-2xl 2xl:text-3xl 3xl:text-4xl gap-3.5 lg:gap-5">
+    <ul class="mb-2 sm:flex sm:justify-around sm:mb-0 sm:text-[14px] md:text-16 lg:text-[18px] 2xl:text-3xl gap-3.5 2xl:gap-5">
       <DropDown title="Product" :listItems="product" @emit-data="closeNav" />
       <DropDown title="Company" :listItems="company" @emit-data="closeNav" />
       <DropDown title="Connect" :listItems="connect" @emit-data="closeNav" />
     </ul>
     <div
-      class="flex flex-col justify-center sm:flex-row gap-x-0.5  items-center pt-4 border-t border-gray-300 sm:border-none sm:pt-0 sm:text-[14px] lg:text-16 ml-auto"
+      class="flex flex-col justify-center sm:flex-row gap-x-0.5  items-center pt-4 border-t border-gray-300 sm:border-none sm:pt-0 sm:text-[14px] ml-auto"
     >
       <router-link to="/login"><base-button
-        class="flex items-center justify-center border-0 text-[hsl(208,49%,24%)] mb-2 sm:mb-0 w-full sm:w-22 md:w-26 lg:w-30 xl:w-35 2xl:w-40 py-4 xl:py-6 sm:text-[hsl(0,100%,100%)] text-16 sm:text-[12px] md:text-16 lg:text-xl xl:text-2xl 2xl:text-3xl 3xl:text-4xl"
+        class="flex items-center justify-center border-0 text-[hsl(208,49%,24%)] mb-2 sm:mb-0 w-full sm:w-22 md:w-26 lg:w-30 2xl:w-40 py-4 sm:text-[hsl(0,100%,100%)] text-16 sm:text-[12px] md:text-16 lg:text-[18px] 2xl:text-[20px]"
         >Login</base-button
       ></router-link>
       <router-link to="/signup"><base-button
-        class=" w-full py-4 xl:py-6 flex items-center justify-center  sm:w-22 md:w-26 lg:w-30 xl:w-35 2xl:w-40 text-16 sm:text-[12px] md:text-16 lg:text-xl xl:text-2xl 2xl:text-3xl 3xl:text-4xl sm:bg-none bg-linear-to-l from-[hsl(353,100%,62%)] to-[hsl(13,100%,72%)] border-b-[hsl(353,100%,62%)] sm:bg-[hsl(0,100%,100%)] sm:border-none sm:text-[hsl(356,100%,66%)]"
+        class=" w-full py-4 flex items-center justify-center  sm:w-22 md:w-26 lg:w-30 2xl:w-40 text-16 sm:text-[12px] md:text-16 lg:text-[18px]  2xl:text-2xl sm:bg-none bg-linear-to-l from-[hsl(353,100%,62%)] to-[hsl(13,100%,72%)] border-b-[hsl(353,100%,62%)] sm:bg-[hsl(0,100%,100%)] sm:border-none sm:text-[hsl(356,100%,66%)]"
         >Sign Up</base-button
       ></router-link>
     </div>
